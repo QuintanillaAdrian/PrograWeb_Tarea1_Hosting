522f827206a9d577cadf585bfe23094c635ebefa
QuintanillaAdrián <adrianq1299@gmail.com>
2025-03-11 13:14:17 -0600

    Pages x4

---
+++ laravel_hosting/resources/views/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main Page</title>
+</head>
+
+<body>
+    <h1><a href="/main"> Main Page </a></h1>
+    <nav>
+        <ul>
+            <li><a href="/books"></a></li>
+            <li><a href="/publishers"></a></li>
+            <li><a href="/authors"></a></li>
+        </ul>
+    </nav>
+</body>
+</html>
--- laravel_hosting/routes/web.php
+++ laravel_hosting/routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
+});
+
+Route::get('/books', function () {
+    return view('books');
+});
+
+Route::get('/authors', function () {
+    return view('authors');
+});
+
+Route::get('/publishers', function () {
+    return view('publishers');
 });
